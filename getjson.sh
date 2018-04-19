62035456901b5c43b923e9944e3e8bb42c019af4
Ed B <redvepr@gmail.com>
2018-04-19 12:31:18 -0400

    API

---
+++ getjson.sh
@@ -0,0 +1,2 @@
+## get exchangerates json privatbank
+curl -o exch.json 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
--- split.sh
+++ split.sh
@@ -1,4 +1,5 @@
 a=$(cat $1 | wc -l)
 a=$(echo $a/2 | bc)
-# echo $a
+head -n$a $1 > ${1}.part1
+tail -n$a $1 > ${1}.part2
 
